507dd8601a97f3aead190eff37a9ea61f28044b6
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-01 14:41:56 -0400

    Updated from Brython Server: 9/1/2017 2:41:55 PM

--- km2miles.py
+++ km2miles.py
@@ -1,1 +1,7 @@
+#Charlie Goodrich
+#09/01/17
+#km2miles.py - converts km to miles
 
+km = float(input("Number of kilometers "))
+miles = km*(3.107/5.0)
+print("There are", miles, "in 5.0 kilometers")

4b628902df29bda97fbb912c5a5195a0ce312ef4
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-01 14:29:04 -0400

    Updated from Brython Server: 9/1/2017 2:29:03 PM

--- pythagoreanDemo.py
+++ pythagoreanDemo.py
@@ -1,1 +1,9 @@
+#Charlie Goodrich
+#09/01/17
+#pythagoreanDemo.py - program that does the pythagorean theorem
 
+from math import sqrt
+
+a = float(input("Enter leg 1: "))
+b = float(input("Enter leg 2: "))
+print("The hypotenuse is", sqrt(a**2 + b**2))

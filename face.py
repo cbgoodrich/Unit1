3b1bf384ffbc02d42d696274e211a0956b9dee98
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-01 14:35:44 -0400

    Updated from Brython Server: 9/1/2017 2:35:43 PM

--- face.py
+++ face.py
@@ -1,1 +1,10 @@
+#Charlie Goodrich
+#09/01/17
+#face.py - program that prints the face on given handout
 
+print("    xxxxx    ")
+print("   x     x   ")
+print("((   o o   ))")
+print("   |  v  |   ")
+print("   | === |   ")
+print("    -----    ")

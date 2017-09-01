343a43bc448f522f5b8f4c69196a47b4ed10be1d
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-01 14:55:09 -0400

    Updated from Brython Server: 9/1/2017 2:55:08 PM

--- nameAge.py
+++ nameAge.py
@@ -1,1 +1,9 @@
+#Charlie Goodrich
+#09/01/17
+#nameAge.py - tells you how old you will be, how many letters in name
+
+name = input("Enter your first and last name ")
+age = int(input("Enter your age "))
+
+var1, var2 = variable.split()
 

ec5f8929050f00b3e9d752bb31f1d15c0b0aefcd
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-01 15:21:35 -0400

    Updated from Brython Server: 9/1/2017 3:21:35 PM

--- nameAge.py
+++ nameAge.py
@@ -2,8 +2,11 @@
 #09/01/17
 #nameAge.py - tells you how old you will be, how many letters in name
 
-name = input("Enter your first and last name ")
+fullName = input("Enter your first and last name ")
 age = int(input("Enter your age "))
 
-var1, var2 = variable.split()
+var1, var2 = fullName.split()
+print("Your first name has", len(var1), "letters.")
+print("Your last name has", len(var2), "letters.")
+print("You will be", age+1, "years old next year")
 

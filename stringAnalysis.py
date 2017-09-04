142a303bc1ba09500c50533eb13f4dbc1f758d9a
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-04 17:25:16 -0400

    Updated from Brython Server: 9/4/2017 5:25:14 PM

--- stringAnalysis.py
+++ stringAnalysis.py
@@ -1,1 +1,28 @@
+#Charlie Goodrich
+#09/04/17
+#stringAnalysis.py - analyzes a string and gives information about the string
 
+fullSentence = input("Enter a sentence: ")
+lowerSentence = fullSentence.lower()
+
+sentenceList = fullSentence.split()
+characters = len(fullSentence)
+word1 = len(sentenceList[0])
+word2 = len(sentenceList[1])
+word3 = len(sentenceList[2])
+word4 = len(sentenceList[3])
+word5 = len(sentenceList[4])
+word6 = len(sentenceList[5])
+word7 = len(sentenceList[6])
+word8 = len(sentenceList[7])
+word9 = len(sentenceList[8])
+letters = word1+word2+word3+word4+word5+word6+word7+word8+word9
+print("Your sentence has", len(sentenceList), "words,", letters, "letters, and",
+characters, "characters.")
+
+
+letterSearch = input("Enter a letter search for: ")
+print(lowerSentence.count(letterSearch))
+
+wordSearch = input("Enter a word search for: ")
+print(lowerSentence.count(wordSearch))

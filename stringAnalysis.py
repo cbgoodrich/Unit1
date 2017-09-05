0fc3d43da6fbe26777a2491afe243f92e9ba3e3a
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-05 14:17:41 -0400

    Updated from Brython Server: 9/5/2017 2:17:40 PM

--- stringAnalysis.py
+++ stringAnalysis.py
@@ -5,21 +5,11 @@
 fullSentence = input("Enter a sentence: ")
 lowerSentence = fullSentence.lower()
 
-sentenceList = fullSentence.split()
 characters = len(fullSentence)
-word1 = len(sentenceList[0])
-word2 = len(sentenceList[1])
-word3 = len(sentenceList[2])
-word4 = len(sentenceList[3])
-word5 = len(sentenceList[4])
-word6 = len(sentenceList[5])
-word7 = len(sentenceList[6])
-word8 = len(sentenceList[7])
-word9 = len(sentenceList[8])
-letters = word1+word2+word3+word4+word5+word6+word7+word8+word9
-print("Your sentence has", len(sentenceList), "words,", letters, "letters, and",
-characters, "characters.")
 
+print("Your sentence has", fullSentence.count(" ") + 1, "words,", 
+ len(fullSentence) - fullSentence.count(" "), "letters, and", len(fullSentence),
+ "characters.")
 
 letterSearch = input("Enter a letter search for: ")
 print(lowerSentence.count(letterSearch))

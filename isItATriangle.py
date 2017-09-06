85b6133521856666bf647c4d537e967fe9b52e75
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-06 14:40:03 -0400

    Updated from Brython Server: 9/6/2017 2:40:02 PM

--- isItATriangle.py
+++ isItATriangle.py
@@ -1,1 +1,15 @@
+#Charlie Goodrich
+#09/06/17
+#isItATriangle.py - tells you if you have a triangle
+
+side1 = float(input("Enter side A: "))
+side2 = float(input("Enter side B: "))
+side3 = float(input("Enter side C: "))
+
+if side1 + side2 > side3 and side1 + side3 > side2 and side2 + side3 > side1:
+    print("You've got a triangle!")
+else:
+    print("You don't have a triangle :'(")
+
+
 

fcb3415a69035914ed269a3d78cec38bf8d29c04
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-06 13:57:58 -0400

    Updated from Brython Server: 9/6/2017 1:57:56 PM

--- change.py
+++ change.py
@@ -1,1 +1,15 @@
+#Charlie Goodrich
+#09/06/17
+#change.py - gives you your change
 
+changeTotal = int(input("How many cents do you have? "))
+
+quarterNum = changeTotal//25
+dimeNum = (changeTotal - quarterNum*25)//10
+nickelNum = (changeTotal - quarterNum*25 - dimeNum*10)//5
+pennyNum = (changeTotal - quarterNum*25 - dimeNum*10 - nickelNum*5)//1
+
+print("Quarters:", quarterNum)
+print("Dimes:", dimeNum)
+print("Nickels:", nickelNum)
+print("Pennies:", pennyNum)

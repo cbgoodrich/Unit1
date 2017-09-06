fedfa57df09256ef36bfac50a72ee17aa049f533
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-06 13:47:19 -0400

    Updated from Brython Server: 9/6/2017 1:47:18 PM

--- eggs.py
+++ eggs.py
@@ -1,1 +1,10 @@
+#Charlie Goodrich
+#09/06/17
+#eggs.py - gives you the number of cartons for eggs
 
+eggs = int(input("How many eggs do you have? "))
+carton = int(input("How many cartons do you have? "))
+
+cartonReal = eggs//12 +1
+
+print(cartonReal == carton)

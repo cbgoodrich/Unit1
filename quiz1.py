affdd638680b67275e7893ada7a11820cbc5c64d
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-11 14:23:35 -0400

    Updated from Brython Server: 9/11/2017 2:23:34 PM

--- quiz1.py
+++ quiz1.py
@@ -1,1 +1,19 @@
+#Charlie Goodrich
+#09/11/17
+#quiz1.py - my first quiz -- prints name, and lucky number
 
+name = "Charlie Goodrich"
+
+print(name)
+
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+
+numSum = num1 + num2
+numProduct = num1 * num2
+
+luckyNum = (num1 * 7) + (num2 * 7)
+
+print("The sum is", numSum)
+print("The product is", numProduct)
+print("Your lucky number is", luckyNum)
